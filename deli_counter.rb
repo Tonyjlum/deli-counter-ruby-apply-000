128ad6234e090e7a751df6ce82311ccdabb329fe
Tony <tonyjlum@gmail.com>
2018-10-19 16:36:48 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,10 +1,10 @@
 # Write your code here.
 
 def line(katz_deli_line)
-  customers_string = []
   if katz_deli_line.empty?
     puts "The line is currently empty." 
   else
+    customers_string = []
     katz_deli_line.each.with_index { |customer, idx| customers_string << "#{idx + 1}. #{customer}" }
     puts "The line is currently: " + customers_string.join(" ")
   end
